d84e803e56c5ec26aa3bb4bff4f47522881c47fd
meddle <meddlesmerch@gmail.com>
2014-08-25 21:09:47 +0300

    Streams added to the picture

--- js/mvs/view.js
+++ js/mvs/view.js
@@ -11,7 +11,12 @@
     $el: null,
     id: null,
     classes: [],
-    type: 'view'
+    type: 'view',
+    streams: {},
+    lambdas: {},
+    $parent: null,
+    parentView: null,
+    template: null
   };
 
   ProAct.View.idNumber = 1;
@@ -28,24 +33,52 @@
         this.id = 'proact-view-' + ProAct.View.idNumber;
         ProAct.View.idNumber = ProAct.View.idNumber + 1;
       }
+
+      var streamProvider = new P.R.StreamProvider(),
+          functionProvider = new P.R.FunctionProvider(),
+          proObjectProvider = new P.R.ProObjectProvider(),
+          lambda;
+
+      this.registry = new ProAct.Registry()
+        .register('s', streamProvider)
+        .register('po', proObjectProvider)
+        .register('obj', proObjectProvider)
+        .register('f', functionProvider)
+        .register('l', functionProvider);
+
+      for (lambda in this.lambdas) {
+        this.registry.store('l:' + lambda, this.lambdas[lambda]);
+      }
     },
 
     bindModel: function (model) {
       if (!this.model) {
         ProAct.proxy(this, model, {
           $el: 'noprop',
-          model: 'noprop'
+          $parent: 'noprop',
+          model: 'noprop',
+          streams: 'noprop',
+          lambdas: 'noprop',
+          registry: 'noprop',
+          template: 'noprop',
         }, {});
 
         this.model = model;
       }
     },
 
+    beforeRender: function ($el) {
+    },
+
     render: function (model) {
       this.bindModel(model);
 
       if (!this.$el) {
-        if (this.id) {
+        if (this.template) {
+          this.$el = $(this.template);
+        }
+
+        if (this.id && (!this.$el || this.$el.length === 0)) {
           this.$el = $(this.el + '#' + this.id);
         }
 
@@ -60,13 +93,59 @@
         this.$el = this.$el.first();
       }
 
+      this.beforeRender(this.$el);
+
       var view = this,
-          $bindings = this.$el.find('[pro-bind]');
+          $bindings = this.$el.find('[pro-bind]'),
+          streams = this.streams,
+          streamPath, action, $actionEl,
+          streamData, i, ln;
+
+      this.classes.core.on(function (event) {
+        var op    = event.args[0],
+            ind   = event.args[1],
+            ov    = event.args[2],
+            nv    = event.args[3],
+            nvs, ovs, i, ln,
+            slice = Array.prototype.slice,
+            operations = ProAct.Array.Operations;
+        if (op === operations.add) {
+          nvs = slice.call(nv, 0);
+          ln = nvs.length;
+          for (i = 0; i < ln; i++) {
+            view.$el.addClass(nvs[i]);
+          }
+        } else if (op === operations.remove) {
+          view.$el.removeClass(ov);
+        } else if (op === operations.splice) {
+          nvs = slice.call(nv, 0);
+          ln = nvs.length;
+
+          for (i = 0; i < ln; i++) {
+            view.$el.addClass(nvs[i]);
+          }
+
+          ovs = slice.call(ov, 0);
+          ln = ovs.length;
+
+          for (i = 0; i < ln; i++) {
+            view.$el.removeClass(ovs[i]);
+          }
+        }
+      });
 
       $bindings.each(function () {
         var $binding = $(this),
             property = $binding.attr('pro-bind'),
+            tag = $binding.prop('tagName').toLowerCase(),
+            oneWay = (tag !== 'input'),
             updating = false;
+
+        if (property.substring(0, 7) === 'one-way') {
+          property = property.substring(8);
+          oneWay = true;
+        }
+
 
         // ->
         view.p(property).on(function () {
@@ -74,26 +153,92 @@
             return;
           }
 
-          if ($binding.attr('type') === 'checkbox') {
+          if (tag !== 'input') {
+            $binding.text(view[property]);
+          } else if ($binding.attr('type') === 'checkbox') {
             $binding.prop('checked', view[property]);
+          } else {
+            $binding.prop('value', view[property]);
           }
         });
 
+        // sync
+        view.p(property).update();
+
+        if (oneWay) {
+          return;
+        }
+
+        // <-
         $binding.on('change', function () {
           try {
             updating = true;
             if ($binding.attr('type') === 'checkbox') {
               view[property] = $binding.prop('checked');
+            } else {
+              view[property] = $binding.prop('value');
             }
           } finally {
             updating = false;
           }
         });
 
-
       });
 
-      // rendering logic here!
+      for (streamPath in streams) {
+        $actionEl = this.$el.find(streamPath)
+        for (action in streams[streamPath]) {
+          streamData = streams[streamPath][action];
+          if (ProAct.Utils.isArray(streamData[0])) {
+            ln = streamData.length;
+            for (i = 0; i < ln; i++) {
+              this.setupStream.apply(this, [$actionEl, action].concat(streamData[i]));
+            }
+          } else {
+            this.setupStream.apply(this, [$actionEl, action].concat(streamData));
+          }
+        }
+      }
+
+      if (this.template && this.$parent) {
+        this.$parent.append(this.$el);
+      }
+
+      this.afterRender(this.$el);
+    },
+
+    afterRender: function ($el) {
+    },
+
+    setupStream: function ($actionEl, action, streamData, propertyName) {
+      var streamArgs = ['s:' + ProAct.Utils.uuid(), streamData],
+          view = this,
+          args = Array.prototype.slice.call(arguments, 2),
+          prop, stream;
+
+      if (propertyName) {
+        prop = this.p(propertyName);
+        if (prop.type() === ProAct.Property.Types.array) {
+          prop = prop.get().core;
+
+        }
+
+        if (prop.target) {
+          prop = prop.target;
+        }
+        streamArgs.push(prop);
+      }
+
+      if (args.length) {
+        streamArgs = streamArgs.concat(args);
+      }
+      stream = this.registry.make.apply(this.registry, streamArgs);
+      $actionEl.on(action, function (e) {
+        e.proView = view;
+        stream.trigger(e);
+      });
+
+      return stream;
     }
   };
 
