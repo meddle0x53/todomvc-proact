aa6910b9fae4e5c117930fcd368f4cce903a2723
meddle <meddlesmerch@gmail.com>
2014-09-03 00:03:47 +0300

    Local storage implementation

--- js/mvs/storage.js
+++ js/mvs/storage.js
@@ -27,18 +27,34 @@
 	  },
 
     create: function (model) {
-      this.update(model, [model.constructor.uuid]);
+      this.update(model, [model.constructor.type ? model.constructor.type : model.constructor.uuid]);
+
+      ProAct.flow.run(function () {
+        model.isCreated = true;
+        model.isSaved = true;
+        model.isDestroyed = false;
+      });
 
       return model;
     },
-    save: function (model) { return model; },
+    save: function (model) {
+      model.isSaved = true;
+      return model;
+    },
     read: function (uuid, query) {
       if (ProAct.Storage.currentCaller) {
         this.on(uuid, ProAct.Storage.currentCaller);
       }
     },
     destroy: function (model) {
-      this.update(ProAct.Event.simple('array', 'del', model), [model.constructor.uuid]);
+
+      ProAct.flow.run(function () {
+        model.isCreated = false;
+        model.isSaved = false;
+        model.isDestroyed = true;
+      });
+
+      this.update(ProAct.Event.simple('array', 'del', model), [model.constructor.type ? model.constructor.type : model.constructor.uuid]);
 
       return model;
     },
@@ -56,22 +72,19 @@
     constructor: ProAct.MemStorage,
 
     create: function (model) {
-      var uuid = model.constructor.uuid,
+      var uuid = model.constructor.type ? model.constructor.type : model.constructor.uuid,
           storage = this.register(uuid);
 
       storage.push(model);
-      model.isCreated = true;
-      model.isSaved = true;
 
       return ProAct.Storage.prototype.create.call(this, model);
     },
 
     destroy: function (model) {
-      var uuid = model.constructor.uuid,
+      var uuid = model.constructor.type ? model.constructor.type : model.constructor.uuid,
           storage = this.register(uuid);
 
       ProAct.Utils.remove(storage, model);
-      model.isSaved = false;
 
       return ProAct.Storage.prototype.destroy.call(this, model);
     },
@@ -90,7 +103,99 @@
       }
 
       return this.store[uuid];
-    }
+    },
+
+    types: {}
+  });
+
+  ProAct.LocalStorage = ProAct.LocalStorage || function () {
+    ProAct.MemStorage.call(this);
+  };
+
+  ProAct.LocalStorage.prototype = ProAct.Utils.ex(Object.create(ProAct.MemStorage.prototype), {
+    constructor: ProAct.LocalStorage,
+
+    toSimpleModel: function (model) {
+      var properties = model.p().properties,
+          p, prop, res = {},
+          systemProps = ['isDestroyed', 'isSaved', 'isCreated', 'shouldDestroy', 'shouldSave'];
+
+      for (p in properties) {
+        prop = properties[p];
+
+        if (prop.type() !== ProAct.Property.Types.auto && systemProps.indexOf(p) === -1) {
+          res[p] = prop.val;
+        }
+      }
+
+      return res;
+    },
+
+    updateLocalStorage: function (uuid) {
+
+      if (window.localStorage) {
+        var self = this,
+            storage = this.register(uuid),
+            json = JSON.stringify(storage.map(function (model) {
+              return self.toSimpleModel(model);
+            }));
+        localStorage.setItem(uuid, json);
+      }
+    },
+
+    create: function (model) {
+      ProAct.MemStorage.prototype.create.call(this, model);
+
+      this.updateLocalStorage(model.constructor.type ? model.constructor.type : model.constructor.uuid);
+      return model;
+    },
+
+    destroy: function (model) {
+      ProAct.MemStorage.prototype.destroy.call(this, model);
+
+      this.updateLocalStorage(model.constructor.type ? model.constructor.type : model.constructor.uuid);
+      return model;
+    },
+
+    read: function (uuid, query) {
+      if (!window.localStorage) {
+        return ProAct.Storage.prototype.read.call(this, uuid, query);
+      }
+
+      ProAct.Storage.prototype.read.call(this, uuid, query);
+
+      var self = this,
+          json = window.localStorage.getItem(uuid),
+          type = this.types[uuid],
+          storage = this.register(uuid);
+
+      if (!json || !type) {
+        return storage;
+      }
+
+      storage = JSON.parse(json);
+
+      this.store[uuid] = storage.map(function (model) {
+        var typed = type.create(model, self);
+
+        ProAct.flow.run(function () {
+          typed.isCreated = true;
+          typed.isSaved = true;
+          typed.isDestroyed = false;
+        });
+
+        return typed;
+      });
+
+      return [].concat(this.store[uuid]);
+    },
+
+    save: function (model) {
+      ProAct.MemStorage.prototype.save.call(this, model);
+
+      this.updateLocalStorage(model.constructor.type ? model.constructor.type : model.constructor.uuid);
+      return model;
+    },
   });
 
 })( window, ProAct );
