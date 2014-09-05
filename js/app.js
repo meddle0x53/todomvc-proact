e7d0de19e0c673918929b565bfcd14c6aa2bebe9
meddle <meddlesmerch@gmail.com>
2014-09-05 10:33:55 +0300

    All the new logix is used in the app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-(function( window ) {
+(function(window, ProAct) {
 	'use strict';
 
   window.app = window.app || {};
 
-  app.Task = ProAct.Model.extend({
+  window.app.Task = ProAct.Model.extend({
     done: false,
     description: ''
   });
+  window.app.Task['type'] = 'Task';
 
   var template = '<li id="test">' +
                   '<div class="view">' +
@@ -17,11 +18,20 @@
                   '<input class="edit" value="Rule the web" pro-bind="one-way:description">' +
                 '</li>';
 
-  app.TaskView = ProAct.View.extend({
+  window.app.TaskView = ProAct.View.extend({
     template: template,
     lambdas: {
       editing: function () {
         return 'editing';
+      },
+      isEditing: function (e) {
+        if (e.proView) {
+          return e.proView.classes._array.indexOf('editing') !== -1;
+        }
+        return e.target._array.indexOf('editing') !== -1;
+      },
+      isNotEditing: function (e) {
+        return e.target._array.indexOf('editing') === -1;
       },
       complete: function (val) {
         if (val) {
@@ -38,29 +48,59 @@
       },
       val: function (event) {
         return $(event.target).prop('value');
+      },
+      focus: function () {
+        this.$el.find('input.edit').focus();
+      },
+      restore: function () {
+        this.$el.find('input.edit').val(this.description);
       }
     },
     streams: {
       'label.description': {
-        dblclick: ['map(l:editing)|>>($1)', 'classes']
+        dblclick: ['map(l:editing)', 'classes']
       },
       'input.edit': {
         keydown: [
-          ['filter(l:enter)|map(pop)|>>($1)', 'classes'],
-          ['filter(l:esc)|map(pop)|>>($1)', 'classes'],
-          ['filter(l:enter)|map(l:val)|>>($1)', 'description']
-        ]
+          ['filter(l:enter)|map(l:val)', 'description'],
+          ['filter(l:enter)|map(pop)', 'classes'],
+          ['filter(l:esc)|map(pop)', 'classes'],
+          ['filter(l:enter)', 'model.doSave']
+        ],
+        blur: ['filter(l:isEditing)|map(pop)', 'classes']
       },
       'button.destroy': {
-        click: ['map(true)|>>($1)', 'isDestroyed']
+        click: ['map(true)', 'model.shouldDestroy']
       }
     },
     pipes: [
-      ['done', 'map(eventToVal)|map(l:complete)', 'classes']
+      ['done', 'map(eventToVal)|map(l:complete)', 'classes'],
+      ['done', '', 'model.doSave'],
+      ['classes', 'filter(l:isEditing)', 'l:focus'],
+      ['classes', 'filter(l:isNotEditing)', 'l:restore'],
     ]
   });
 
-  app.TaskViews = ProAct.Views.extend({
+  window.app.NewTaskView = ProAct.NewView.extend({
+    el: 'header',
+    id: 'header',
+    lambdas: {
+      enter: function (event) {
+        return event.keyCode === 13;
+      },
+    },
+    streams: {
+      'input#new-todo': {
+        keydown: [
+          ['filter(l:enter)|map(true)', 'shouldCreate'],
+        ]
+      },
+    },
+    pipes: [
+    ]
+  });
+
+  window.app.TaskViews = ProAct.Views.extend({
     el: 'section',
     id: 'todoapp',
     itemsEl: 'ul',
@@ -88,7 +128,7 @@
     },
     lambdas: {
       completedCountToClass: function (e) {
-        if (e.args[0].completedItems.length === 0) {
+        if (e.target.length === 0) {
           return 'hidden';
         }
 
@@ -100,44 +140,86 @@
         }
 
         return ProAct.Event.simple('array', 'del', 'hidden');
+      },
+      done: function (model) {
+        return model.done;
+      },
+      active: function (model) {
+        return !model.done;
+      },
+      doneFilter: function (e) {
+        if (e.args[2] === 'l:done') {
+          return 'selected';
+        }
+        return ProAct.Event.simple('array', 'del', 'selected');
+      },
+      activeFilter: function (e) {
+        if (e.args[2] === 'l:active') {
+          return 'selected';
+        }
+        return ProAct.Event.simple('array', 'del', 'selected');
+      },
+      allFilter: function (e) {
+        if (e.args[2] === 'l:truth') {
+          return 'selected';
+        }
+        return ProAct.Event.simple('array', 'del', 'selected');
+      }
+    },
+    streams: {
+      'button#clear-completed': {
+        click: [
+          ['map(true)', 'models.[l:done].shouldDestroy']
+        ]
       }
     },
     pipes: [
       ['completedItems', 'map(l:completedCountToClass)', 'btnCompletedClasses'],
       ['models', 'map(l:itemsCountToClass)', 'footerClass'],
-      ['doneAll', 'filter(defined)', 'models.[].done']
+      ['doneAll', 'filter(defined)', 'models.[].done'],
+      ['filter', 'map(l:doneFilter)', 'completedSelected'],
+      ['filter', 'map(l:activeFilter)', 'activeSelected'],
+      ['filter', 'map(l:allFilter)', 'allSelected']
     ]
   });
 
-  app.storage = new ProAct.MemStorage();
+  window.app.storage = new ProAct.LocalStorage();
 
-  app.model = app.Task.create({
-    done: false,
-    description: 'Feed Dally with 60ml milk.'
-  }, app.storage);
+  window.app.getTaskListView = function (filter) {
+    if (!window.app.views) {
+      window.app.views = new window.app.TaskViews();
+      window.app.models = ProAct.Models.create(window.app.Task, window.app.storage);
+      window.app.views.render(window.app.models);
 
-  app.model2 = app.Task.create({
-    done: true,
-    description: 'Return to tanya'
-  }, app.storage);
+      window.app.newView = new window.app.NewTaskView();
+      window.app.newView.render(window.app.Task.create({
+        done: false,
+        description: ''
+      }, window.app.storage));
+    }
 
-  app.model3 = app.Task.create({
-    done: false,
-    description: 'Dring beer.'
-  }, app.storage);
+    if (filter) {
+      if (P.U.isFunction(filter)) {
+        window.app.views.items.core.filteringListener(filter);
+      } else {
+        window.app.views.filter = filter;
+      }
+    }
 
-  app.model4 = app.Task.create({
-    done: false,
-    description: 'Fighting...'
-  }, app.storage);
+    return window.app.views;
+  };
 
-  app.model.save();
-  app.model2.save();
-  app.model3.save();
-  app.model4.save();
 
-  app.views = new app.TaskViews();
-  app.models = ProAct.Models.create(app.Task, app.storage);
-  app.views.render(app.models);
+  window.app.router = new ProAct.Router();
+  window.app.router
+    .route(function () {
+      window.app.getTaskListView('l:truth');
+    })
+    .route(/completed/, function () {
+      window.app.getTaskListView('l:done');
+    })
+    .route(/active/, function () {
+      window.app.getTaskListView('l:active');
+    }).start();
 
-})( window );
+})(window, ProAct);
