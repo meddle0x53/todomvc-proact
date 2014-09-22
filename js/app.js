7e90deabe8be5abab24e9d968a804684636047dd
meddle <meddlesmerch@gmail.com>
2014-09-23 00:12:41 +0300

    Cleaner app.js

--- js/app.js
+++ js/app.js
@@ -2,34 +2,35 @@
 	'use strict';
 
   window.app = window.app || {};
+  var app = window.app;
 
-  window.app.storage = new ProAct.LocalStorage();
+  app.storage = new ProAct.LocalStorage();
 
-  window.app.getTaskListView = function (filter) {
-    if (!window.app.views) {
-      window.app.views = new window.app.TasksView();
-      window.app.models = ProAct.Models.create(window.app.Task, window.app.storage);
-      window.app.views.render(window.app.models);
+  app.getTaskListView = function (filter) {
+    if (!app.views) {
+      app.views = new app.TasksView();
+      app.models = ProAct.Models.create(app.Task, app.storage);
+      app.views.render(app.models);
 
-      window.app.newView = new window.app.NewTaskView();
-      window.app.newView.render(window.app.Task.create({}, window.app.storage));
+      app.newView = new app.NewTaskView();
+      app.newView.render(app.Task.create({}, app.storage));
     }
 
-    window.app.views.setFilter(filter);
+    app.views.setFilter(filter);
 
-    return window.app.views;
+    return app.views;
   };
 
-  window.app.router = new ProAct.Router('/labs/architecture-examples/proact/');
-  window.app.router
+  app.router = new ProAct.Router('/labs/architecture-examples/proact/');
+  app.router
     .route(function () {
-      window.app.getTaskListView('l:truth');
+      app.getTaskListView('l:truth');
     })
     .route(/completed/, function () {
-      window.app.getTaskListView('l:done');
+      app.getTaskListView('l:done');
     })
     .route(/active/, function () {
-      window.app.getTaskListView('l:active');
+      app.getTaskListView('l:active');
     }).start();
 
 })(window, ProAct);
